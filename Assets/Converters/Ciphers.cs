4741cb3e0b717913aa54b6ad6b93cc04a1841262
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-10-23 21:35:49 -0400

    I don't even know

--- Assets/Converters/Ciphers.cs
+++ Assets/Converters/Ciphers.cs
@@ -3,20 +3,117 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-public class Ciphers {
+public static class Ciphers {
 	public const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	public string EncryptCaesarCipher(string message, int key, string alphabet = alphabet)
+    private class Tuple<T1, T2>
     {
+        private T1 _t1;
+        private T2 _t2;
+        public Tuple(T1 t1, T2 t2)
+        {
+            _t1 = t1;
+            _t2 = t2;
+        }
+        public object this[int ix]
+        {
+            get
+            {
+                if (ix == 0)
+                    return _t1;
+                else if (ix == 1)
+                    return _t2;
+                else throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (ix == 0)
+                {
+                    if (value is T1)
+                        _t1 = (T1)value;
+                    else throw new InvalidCastException(String.Format("Cannot cast to " + typeof(T1)));
+                }
+                else if (ix == 1)
+                {
+                    if (value is T2)
+                        _t2 = (T2)value;
+                    else throw new InvalidCastException(String.Format("Cannot cast to " + typeof(T2)));
+                }
+                else throw new IndexOutOfRangeException();
+            }
+        }
+    }
+    private class Grid
+    {
+        public string contents;
+        char replacedLetter;
+        char replacementLetter;
+
+    }
+    public static string CreateAlphabet(string key = "", bool atEnd = false, string alphabet = alphabet)
+    {
+        key = key.ToUpper();
+        alphabet = alphabet.ToUpper();
+        if (alphabet.Count() != 26)
+            throw new ArgumentOutOfRangeException("Alphabet is not 26 letters long.");
+        if (alphabet.Distinct().Count() != 26)
+            throw new ArgumentException("Alphabet has duplicate characters.");
+        if (key.Any(x => !Char.IsLetter(x)))
+            throw new ArgumentException("Key contains non-letter characters.");
+        if (alphabet.Any(x => !Char.IsLetter(x)))
+            throw new ArgumentException("Key contains non-letter characters.");
+        var keyUnique = key.Distinct();
+        var alphaUnique = alphabet.Where(ch => !keyUnique.Contains(ch));
+        var resultSeq = atEnd ?
+                        alphaUnique.Concat(keyUnique) :
+                        keyUnique.Concat(alphaUnique);
+        return resultSeq.Join("");
+    }
+    public static string CreateGrid(string key = "", bool atEnd = false, char replacedLetter = 'J', char replacementLetter = 'I', string alphabet = alphabet)
+    {
+
+    }
+
+    #region Caesar
+    public static string EncryptCaesarCipher(string message, int key, string alphabet = alphabet)
+    {
+        alphabet = CreateAlphabet("", false, alphabet);
         return message.Select(ch => EncryptCaesarCipher(ch, key, alphabet)).Join("");   
     }
-    public string DecryptCaesarCipher(string message, int key, string alphabet = alphabet)
+    public static string DecryptCaesarCipher(string message, int key, string alphabet = alphabet)
     {
         return EncryptCaesarCipher(message, -key, alphabet);
     }
-    private char EncryptCaesarCipher(char letter, int key, string alphabet)
+    public static string EncryptRot13(string message, string alphabet = alphabet)
     {
-        int ix = alphabet.IndexOf(letter);
-        return alphabet[(ix + key + 26) % 26];
+        return EncryptCaesarCipher(message, 13, alphabet);
     }
+    private static char EncryptCaesarCipher(char letter, int key, string alphabet)
+    {
+        if (!Char.IsLetter(letter))
+            throw new ArgumentException("Non-letter found in message.");
+        int ix = alphabet.IndexOf(Char.ToUpper(letter));
+        char result = alphabet[(ix + key + 26) % 26];
+        return Char.IsLower(letter) ? Char.ToLower(result) : Char.ToUpper(result);
+        
+    }
+    #endregion
+    #region Atbash
+    public static string EncryptAtbash(string message, string alphabet = alphabet)
+    {
+        alphabet = CreateAlphabet("", false, alphabet);
+        return message.Select(ch => EncryptAtbash(ch, alphabet)).Join("");
+    }
+    private static char EncryptAtbash(char ch, string alphabet)
+    {
+        if (!Char.IsLetter(ch))
+            throw new ArgumentException("Non-letter found in message.");
+        int initIx = alphabet.IndexOf(Char.ToUpper(ch));
+        char result = alphabet[27 - initIx];
+        return Char.IsLower(ch) ? Char.ToLower(result) : Char.ToUpper(result);
+    }
+    #endregion
+    #region Playfair
+
+    #endregion
 }
---
+++ Colores/Assets/Colors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colors : MonoBehaviour {
+
+	public Color[] colors = new Color[16];
+	// Use this for initialization
+	void Start () {
+        for (int ix = 0; ix < 16; ix++)
+        {
+			colors[ix] = Color.HSVToRGB((float)ix / 16, 1, 1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
